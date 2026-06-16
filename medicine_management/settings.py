9176d52c86fad952e8295f6a61652ff843a82766
imran <uffmink@gmail.com>
2026-06-16 11:53:19 +0300

    feat: Update ALLOWED_HOSTS to include production domain

--- medicine_management/settings.py
+++ medicine_management/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', 'medi-care-nine-theta.vercel.app'])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
